f4c9959e90482d60b10180a892a511afb6d1c339
Patryk <patrykowo@gmail.com>
2022-12-11 00:35:17 +0100

    Create migration and seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\CompanySeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
         \App\Models\User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@page.com',
+        ]);
+
+        $this->call([
+            CompanySeeder::class,
         ]);
     }
 }
