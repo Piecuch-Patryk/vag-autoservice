b1a28334f96671ee4698d5a3e45eef59fd9e05e4
Patryk <patrykowo@gmail.com>
2022-12-10 20:38:38 +0100

    Change some styles in auth pages. Add some content to those pages.

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -10,15 +10,15 @@
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            Link weryfikacyjny został wysłany na podany adres email.
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    Przed kontynuowaniem, sprawdź swój adres email.
+                    Jeśli nie otrzymałeś wiadomości,
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">kliknij tutaj, aby przesłać ponownie.</button>.
                     </form>
                 </div>
             </div>
