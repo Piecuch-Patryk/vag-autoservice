50b931f8a24e8c92055ce3496cf8f22b42b8770d
Patryk <patrykowo@gmail.com>
2022-12-10 19:40:27 +0100

    Add language file - polish.

---
+++ lang/pl/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Nie znaleziono takich danych uwierzytelniających.',
+    'password' => 'Podane hasło jest niepoprawne',
+    'throttle' => 'Za dużo prób logowania. Prosimy spróbować ponownie za :seconds sekund.',
+
+];
