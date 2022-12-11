fb7f56fe5b134e080842dfc80c7191e65fd4312a
Patryk <patrykowo@gmail.com>
2022-12-11 13:55:10 +0100

    Create migration.

---
+++ database/migrations/2022_12_11_123821_create_invoices_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('number');
+            $table->string('make');
+            $table->string('model');
+            $table->string('registration');
+            $table->bigInteger('vin');
+            $table->bigInteger('milage');
+            $table->text('description');
+            $table->text('jobs');
+            $table->text('parts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
