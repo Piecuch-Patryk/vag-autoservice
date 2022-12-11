d5e5a29398cabaa88b35708f42e9e904168e64c6
Patryk <patrykowo@gmail.com>
2022-12-11 19:35:36 +0100

    Fix navigation link.

--- resources/views/navs/nav.blade.php
+++ resources/views/navs/nav.blade.php
@@ -1,5 +1,5 @@
 <a class="nav-link {{ Route::currentRouteName() == 'home' ? 'active' : '' }}" aria-current="page" href="{{ route('home') }}">Profil: {{ auth()->user()->name }}</a>
 <a class="nav-link {{ Route::currentRouteName() == 'company.edit' ? 'active' : '' }}" href="{{ route('company.edit') }}">Dane Firmy</a>
-<a class="nav-link {{ Route::currentRouteName() == 'invoice.create' ? 'active' : '' }}" href="{{ route('invoice.create') }}">Lista Zleceń</a>
-<a class="nav-link" href="#">Utwórz Zlecenie</a>
+<a class="nav-link" href="">Lista Zleceń</a>
+<a class="nav-link {{ Route::currentRouteName() == 'invoice.create' ? 'active' : '' }}" href="{{ route('invoice.create') }}">Utwórz Zlecenie</a>
 <a class="nav-link" href="#">Opinie Klientów</a>
