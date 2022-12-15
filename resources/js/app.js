386ae140f5ecf8db63154908c305a6c17d5a9474
Patryk <patrykowo@gmail.com>
2022-12-15 04:08:03 +0100

    Change navigation to use modal. Remove unused js scripts.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,1 @@
 import './bootstrap';
-
-
-// Toggle Page Navigation
-function toggleNav() {
-  const collapsable = document.getElementById('collapsable-nav');
-
-  if(collapsable.classList.contains('opened')) collapsable.classList.remove('opened');
-  else collapsable.classList.add('opened');
-
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('[data-nav-btn="toggle"]').forEach(btn => btn.addEventListener('click', toggleNav));
-
-  document.getElementById('collapsable-nav').classList.add('closed');
-});
--- resources/views/frontend/navigation/main.blade.php
+++ resources/views/frontend/navigation/main.blade.php
@@ -12,7 +12,8 @@
                 {{ $company->city }} ul. {{ $company->street }} {{ $company->number }}
             </p>
         </div>
-        <button data-nav-btn="toggle" class="hamburger-menu-open">
+        <button type="button" class="hamburger-menu-open" data-bs-toggle="modal"
+            data-bs-target="#navModal">
             <span></span>
             <span></span>
             <span></span>
@@ -25,43 +26,44 @@
             <button class="btn btn-outline-primary py-0 shadow-sm">Szukaj</button>
         </form>
     </div>
+    <div class="modal fade" id="navModal" tabindex="-1" aria-labelledby="navModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header border-0">
+                    <div>
+                        <img src="{{ Storage::url('logo/black.png') }}" alt="">
+                    </div>
+                    <div class="d-flex flex-column align-items-center text-center flex-fill">
+                        <a href="tel:{{ $company->phone }}"
+                            class="text-custom-secondary text-decoration-none custom-text-shadow">{{ $company->phone }}</a>
+                    </div>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <ul class="nav justify-content-around mt-3 border-bottom">
+                        <li class="nav-item">
+                            <a class="nav-link custom-text-shadow" href="#">O Nas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link custom-text-shadow" href="#">Opinie</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link custom-text-shadow" href="#" tabindex="-1"
+                                aria-disabled="true">Kontakt</a>
+                        </li>
+                    </ul>
+                    <ul class="nav flex-column mt-3 custom-bg-image">
 
-    <div id="collapsable-nav" class="bg-white fixed-top p-3 collapsable-nav">
-        <div class="d-flex justify-content-between align-items-center">
-            <div>
-                <img src="{{ Storage::url('logo/black.png') }}" alt="">
+                        @foreach ($categories as $category)
+                            <li class="nav-item text-center">
+                                <a class="nav-link custom-text-shadow"
+                                    href="{{ route('category.show', $category->id) }}">{{ $category->catName }}</a>
+                            </li>
+                        @endforeach
+
+                    </ul>
+                </div>
             </div>
-            <div class="d-flex flex-column align-items-center text-center">
-                <a href="tel:{{ $company->phone }}"
-                    class="text-custom-secondary text-decoration-none custom-text-shadow">{{ $company->phone }}</a>
-            </div>
-            <button data-nav-btn="toggle" class="hamburger-menu-close">
-                <span></span>
-                <span></span>
-            </button>
-        </div>
-
-        <div>
-            <ul class="nav justify-content-around mt-3 border-bottom">
-                <li class="nav-item">
-                    <a class="nav-link custom-text-shadow" href="#">O Nas</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link custom-text-shadow" href="#">Opinie</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link custom-text-shadow" href="#" tabindex="-1" aria-disabled="true">Kontakt</a>
-                </li>
-            </ul>
-			<ul class="nav flex-column mt-3 custom-bg-image">
-
-				@foreach ($categories as $category)
-                <li class="nav-item text-center">
-					<a class="nav-link custom-text-shadow" href="{{ route('category.show', $category->id) }}">{{ $category->catName }}</a>
-                </li>
-				@endforeach
-
-            </ul>
         </div>
     </div>
 </nav>
