2c0c04944a6b9c98f8ab71caee975ddee9613850
Patryk <patrykowo@gmail.com>
2022-12-15 03:44:39 +0100

    Add missing text shadow.

--- resources/views/frontend/shared/footer.blade.php
+++ resources/views/frontend/shared/footer.blade.php
@@ -6,19 +6,19 @@
         <div class="col-6 text-center">
             <div class="py-1">
 				<h5 class="fs-6 mb-0">Lokalizacja</h5>
-				<p class="mb-0 lh-1 custom-fs-8">{{ $company->post_code }}</p>
-				<p class="mb-0 lh-1 custom-fs-8">{{ $company->city }}</p>
-				<p class="mb-0 lh-1 custom-fs-8">{{ $company->street }} {{ $company->number }}</p>
+				<p class="mb-0 lh-1 custom-fs-8 custom-text-shadow">{{ $company->post_code }}</p>
+				<p class="mb-0 lh-1 custom-fs-8 custom-text-shadow">{{ $company->city }}</p>
+				<p class="mb-0 lh-1 custom-fs-8 custom-text-shadow">{{ $company->street }} {{ $company->number }}</p>
 			</div>
 			<div class="py-1 d-flex flex-column">
 				<h5 class="fs-6 mb-0">Kontakt</h5>
-				<a href="tel:{{ $company->phone }}" class="mb-0 lh-1 custom-fs-8 text-decoration-none text-dark">{{ $company->phone }}</a>
+				<a href="tel:{{ $company->phone }}" class="mb-0 lh-1 custom-fs-8 custom-text-shadow text-decoration-none text-dark">{{ $company->phone }}</a>
 			</div>
         </div>
     </div>
 	<div class="row mt-5">
 		<div class="col-12 text-center">
-			<p class="custom-fs-8 text-muted mb-0">Design&Dev @ Patryk Piecuch <a href="https://devpat.online" class="text-muted" target="_blank" rel="noopener noreferrer">devpat.online</a></p>
+			<p class="custom-fs-8 custom-text-shadow text-muted mb-0">Design&Dev @ Patryk Piecuch <a href="https://devpat.online" class="text-muted" target="_blank" rel="noopener noreferrer">devpat.online</a></p>
 		</div>
 	</div>
 </div>
