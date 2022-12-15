304b3536686dbfd333247919641e6e18991dd073
Patryk <patrykowo@gmail.com>
2022-12-15 02:14:45 +0100

    Add another section.

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.frontend')
 
 @section('main')
-	<main class="container px-2 py-4 rounded-2 shadow bg-white custom-section">
-		<div class="row">
+	<main class="container px-2">
+		<div class="row mb-5 py-5 rounded-2 shadow bg-white custom-section">
 			<div class="col-12 text-center">
 				<h2 class="custom-text-shadow fs-3">SPECJALIZUJEMY SIĘ W SERWISOWANIU SAMOCHODÓW Z GRUPY VOLKSWAGEN</h2>
 				<p class="custom-text-shadow">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -10,4 +10,21 @@
 			</div>
 		</div>
 	</main>
+	
+	<section class="container px-4 py-5 custom-section-image">
+		<div class="row">
+			<div class="col-12">
+				<ul class="list-group list-group-flush rounded-2 shadow">
+
+					@foreach ($categories as $category)
+					<li class="list-group-item border-0 custom-text-shadow">
+						<p class="fs-5 mb-0">{{ $category->catName }}</p>
+						<p class="lh-1 mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui eveniet fugit, est quam sit sapiente sequi praesentium accusamus.</p>
+					</li>
+					@endforeach
+
+				</ul>
+			</div>
+		</div>
+	</section>
 @endsection
