7af44db3acbb5b84d6708c78208895aec309329a
Patryk <patrykowo@gmail.com>
2022-12-28 21:54:22 +0100

    Fix display review.

--- resources/views/review/index.blade.php
+++ resources/views/review/index.blade.php
@@ -1,43 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-	<div class="row align-items-center">
-		<div class="col-12 col-md-10 mx-auto mb-3">
-			<h1>Opinie Klientów</h1>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-12 col-md-10 mx-auto mb-3">
+                <h1>Opinie Klientów</h1>
 
-			@include('shared.success')
+                @include('shared.success')
 
-			@if (count($reviews) > 1)
-				<ul class="list-group rounded-0">
+                @if (count($reviews) > 0)
+                    <ul class="list-group rounded-0">
 
-					@foreach ($reviews as $review)
-						<li class="list-group-item text-center mb-3">
-							<div class="d-flex flex-column flex-lg-row justify-content-between">
-								{{ $review->name }}
-								@include('frontend.partials.rated')
-							</div>
-							<div class="text-lg-start">
-								<p>
-									{{ $review->content }}
-								</p>
-							</div>
-							<div class="text-lg-start">
-								<form method="POST" action="{{ route('review.delete', $review->id) }}">
-									@csrf
-									@method('DELETE')
-									<button class="btn btn-sm btn-outline-danger py-0">Usuń</button>
-								</form>
-							</div>
-						</li>
-					@endforeach
+                        @foreach ($reviews as $review)
+                            <li class="list-group-item text-center mb-3">
+                                <div class="d-flex flex-column flex-lg-row justify-content-between">
+                                    {{ $review->name }}
+                                    @include('frontend.partials.rated')
+                                </div>
+                                <div class="text-lg-start">
+                                    <p>
+                                        {{ $review->content }}
+                                    </p>
+                                </div>
+                                <div class="text-lg-start">
+                                    <form method="POST" action="{{ route('review.delete', $review->id) }}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-sm btn-outline-danger py-0">Usuń</button>
+                                    </form>
+                                </div>
+                            </li>
+                        @endforeach
 
-				</ul>
-			@else
-				<p>Brak opinii klientów.</p>
-			@endif
+                    </ul>
+                @else
+                    <p>Brak opinii klientów.</p>
+                @endif
 
-		</div>
-	</div>
-</div>
+            </div>
+        </div>
+    </div>
 @endsection
