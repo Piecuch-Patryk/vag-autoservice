6dfc3cf5a170f03d819d964f861dd37af1166245
Patryk <patrykowo@gmail.com>
2022-12-29 15:07:52 +0100

    Add carousel.

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,17 +1,59 @@
 @extends('layouts.frontend')
 
 @section('main')
-    <main class="container-fluid px-2">
-        <div class="row mb-5 py-5 rounded-2 shadow bg-white custom-section mt-md-5">
-            <div class="col-12 col-lg-8 mx-lg-auto text-center">
-                <h2 class="custom-text-shadow fs-3">SPECJALIZUJEMY SIĘ W SERWISOWANIU SAMOCHODÓW Z GRUPY VOLKSWAGEN</h2>
-                <p class="custom-text-shadow">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-                    in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <button class="btn btn-sm btn-outline-primary py-0" data-bs-target="#contactModal" data-bs-toggle="modal" data-bs-dismiss="modal">Umów Wizytę</button>
+    <main class="custom-negative-margin bg-white mb-5 mt-lg-5 rounded-1">
+                
+        <div id="mainCarousel" class="carousel carousel-dark slide shadow" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#mainCarousel" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#mainCarousel" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
             </div>
+            <div class="carousel-inner">
+                <div class="carousel-item mb-5 p-3 active" data-bs-interval="4000">
+                    <div class="row">
+                        <div class="col-12 col-md-8 col-lg-6 mx-auto custom-section">
+                            <h2 class="custom-text-shadow fs-3">SPECJALIZUJEMY SIĘ W SERWISOWANIU SAMOCHODÓW Z GRUPY VOLKSWAGEN</h2>
+                            <p class="custom-text-shadow">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                                ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                                sunt
+                                in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                <button class="btn btn-sm btn-outline-primary py-0" data-bs-target="#contactModal"
+                                data-bs-toggle="modal" data-bs-dismiss="modal">Umów Wizytę</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item mb-5 p-3" data-bs-interval="4000">
+                    <div class="row">
+                        <div class="col-12 col-md-8 col-lg-6 mx-auto">
+                            <h2 class="custom-text-shadow fs-3">OFERUJEMY USŁUGI W ZAKRESIE TRANSPORTU SAMOCHODÓW NA LAWECIE</h2>
+                            <p class="custom-text-shadow">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                                ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                                in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                <button class="btn btn-sm btn-outline-primary py-0" data-bs-target="#contactModal"
+                                data-bs-toggle="modal" data-bs-dismiss="modal">Umów Wizytę</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev d-none d-md-block" type="button" data-bs-target="#mainCarousel"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Poprzedni</span>
+            </button>
+            <button class="carousel-control-next d-none d-md-block" type="button" data-bs-target="#mainCarousel"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Następny</span>
+            </button>
         </div>
     </main>
 
@@ -55,11 +97,11 @@
                         Setki zadowolonych klientów
                     </li>
                 </ul>
-                <button class="btn btn-sm btn-outline-primary py-0" data-bs-target="#contactModal" data-bs-toggle="modal" data-bs-dismiss="modal">Umów Wizytę</button>
+                <button class="btn btn-sm btn-outline-primary py-0" data-bs-target="#contactModal" data-bs-toggle="modal"
+                    data-bs-dismiss="modal">Umów Wizytę</button>
             </div>
         </div>
     </article>
 
     @include('frontend.partials.reviews')
-    
 @endsection
