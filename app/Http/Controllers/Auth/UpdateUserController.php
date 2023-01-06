85900f8a057854930c139dcbedf9e617cc55a83f
Patryk <patrykowo@gmail.com>
2023-01-06 21:00:55 +0100

    Fix password update method.

--- app/Http/Controllers/Auth/UpdateUserController.php
+++ app/Http/Controllers/Auth/UpdateUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\UpdateUserRequest;
 
 class UpdateUserController extends Controller
@@ -18,8 +19,13 @@ class UpdateUserController extends Controller
     public function update(UpdateUserRequest $request, $id)
     {
         $user = User::find($id);
-        $user->update($request->all());
-        
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'updated_at' => now(),
+        ]);
+
         return redirect()->route('home')->with('success', 'Dane zostały zaktualizowane');
     }
 }
